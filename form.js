c029e98d56899540d10f5877145072eca734f1a8
YasminValverde <yasminvalverde5@gmail.com>
2026-04-17 18:50:11 +0200

    hasta las observaciones

--- form.js
+++ form.js
@@ -1,73 +1,218 @@
-window.onload = function(){
+window.onload = function () {
   const camposNombre = document.querySelectorAll(".nombreCompleto");
   for (let i = 0; i < camposNombre.length; i++) {
     const campo = camposNombre[i];
     campo.addEventListener("blur", validarNom);
   }
-const campsAmbImg = document.getElementsByClassName("ambImg");
+  const campsAmbImg = document.getElementsByClassName("ambImg");
   for (let i = 0; i < campsAmbImg.length; i++) {
-    campsAmbImg[i],addEventListener("blur",ocultarOk);
+    campsAmbImg[i].addEventListener("blur", ocultarOk);
+  }
+  document.formulario.data_naix.addEventListener("blur", validarDataNaixement);
+  document.formulario.email.addEventListener("blur", validarEmail);
+  document.formulario.telefon.addEventListener("blur", validarTelefon);
+  document.formulario.dni.addEventListener("blur", validarDNI);
+  document.formulario.usuari.addEventListener("blur", validarUsuari);
+  document.formulario.password.addEventListener("input", fortalezaContraseña);
+  document.formulario.password.addEventListener("blur", validarContraseña);
+  document.formulario.password2.addEventListener("blur", validarContraseña2);
+  document.formulario.observacions.addEventListener("blur", validarObservacions);
+  document.formulario.observacions.addEventListener("input", comptadorCaracteres);
+
+  
+  var meter = document.getElementById("password-strength-meter");
+
+  // ------------------------------METODOS-----------------------------------
+  function validarNom() {
+    tractaCorrecte(this);
+
+    let nom = this.value;
+    if (nom.length === 0) {
+      tractaError(this, "El campo no puede estar vacío");
+      return;
+    }
+    for (let i = 0; i < nom.length; i++) {
+      if (!isNaN(nom[i])) {
+        tractaError(this, "No puede contener NUMEROS!");
+        ocultarOk(this);
+
+        return;
+      }
+    }
+    mostrarOk(this);
+  }
+
+  function validarDataNaixement() {
+    tractaCorrecte(this);
+    const data = new Date(this.value);
+    if (!this.value || isNaN(data) || data >= new Date()) {
+      tractaError(this, "Data incorrecte");
+      return;
+    }
+    mostrarOk(this);
+  }
+  function validarEmail() {
+    tractaCorrecte(this);
+    const email = this.value;
+    const patron = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (!patron.test(email)) {
+      tractaError(this, "Mail incorrecte");
+      return;
+    }
+    mostrarOk(this);
+  }
+  function validarTelefon() {
+    tractaCorrecte(this);
+    const telefon = this.value;
+    const patron = /^\d{9}$/;
+    if (!patron.test(telefon)) {
+      tractaError(this, "Telèfon incorrecte");
+      return;
+    }
+    mostrarOk(this);
+  }
+
+  function validarDNI() {
+    tractaCorrecte(this);
+    const dni = this.value;
+    const patron = /^\d{8}[a-zA-Z]$/;
+    if (!patron.test(dni)) {
+      tractaError(this, "DNI incorrecte");
+      return;
+    }
+    const lletres = [
+      "T",
+      "R",
+      "W",
+      "A",
+      "G",
+      "M",
+      "Y",
+      "F",
+      "P",
+      "D",
+      "X",
+      "B",
+      "N",
+      "J",
+      "Z",
+      "S",
+      "Q",
+      "V",
+      "H",
+      "L",
+      "C",
+      "K",
+      "E",
+      "T",
+    ];
+    const i = parseInt(dni) % 23;
+    if (dni[8] !== lletres[i]) {
+      tractaError(this, "Lletra DNI incorrecta");
+      return;
+    }
+    mostrarOk(this);
+  }
+
+  function validarUsuari() {
+    ocultarOk(this);
+    tractaCorrecte(this);
+    if (this.value.length < 5) {
+      tractaError(this, "longitud mínima 5 caràcters");
+      return;
+    }
+    if (this.value.length > 12) {
+      tractaError(this, "longitud màxima 12 caràcters");
+      return;
+    }
+    mostrarOk(this);
+  }
+
+  function validarContraseña() {
+    tractaCorrecte(this);
+    ocultarOk(this)
+    if (meter.value < 2) {
+      tractaError(this, "Contrasenya incorrecta");
+      return;
+    }
+    mostrarOk(this);
+  }
+
+  function fortalezaContraseña() {
+    var result = zxcvbn(this.value);
+    meter.value = result.score;
+    habilitarPassword2(result.score);
+  }
+
+  function habilitarPassword2(score) {
+    document.formulario.password2.disabled = score < 2;
+    score=0;
+
+  }
+  function validarContraseña2() {
+    tractaCorrecte(this);
+    ocultarOk(this)
+    if (document.formulario.password.value !== this.value) {
+      tractaError(this, "Les contrasenyes no coincideixen");
+      return;
+    }
+    mostrarOk(this);
     
   }
-document.formulario.data_naix.addEventListener("blur", validarDataNaixement);
-document.formulario.email.addEventListener("blur", validarEmail);
 
-
-
-// ------------------------------METODOS-----------------------------------
-function validarNom() {
-  tractaCorrecte(this)
-
-  let nom = this.value;
-  if (nom.length === 0) {
-    tractaError(this, "El campo no puede estar vacío");
-    return;
-  }
-  for (let i = 0; i < nom.length; i++) {
-    if (!isNaN(nom[i])) {
-      tractaError(this, "No puede contener NUMEROS!");
-      ocultarOk(this)
-
+ function validarObservacions() {
+    tractaCorrecte(this);
+    if (this.value.length >120) {
+      tractaError(this, "longitud màxima 120 caràcters");
       return;
     }
   }
-  mostrarOk(this);
-}
 
-function validarDataNaixement() {
-  tractaCorrecte(this);
-  const data = new Date(this.value);
-  if (!this.value || isNaN(data) || data >= new Date()) {
-    tractaError(this, "Data incorrecte");
-    return;
+  function comptadorCaracteres() {
+    const caracters = document.formulario.observacions.value.length;
+    const comptador = document.getElementById("comptador");
+    comptador.innerHTML = caracters + "/120";
+    if (comptador>120) {
+      comptador.style.color ="red";
+    } else {
+      comptador.style.color ="white";
+    }
   }
-  mostrarOk(this);
-}
-function validarEmail() {
-  tractaCorrecte(this);
-  const email = this.value;
-  const patron= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-  if (!patron.test(email)) {
-    tractaError(this, "Mail incorrecte");
-    return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+  function tractaCorrecte(objecte) {
+    objecte.setCustomValidity("");
   }
-  mostrarOk(this);
-}
 
-function tractaCorrecte(objecte) {
-  objecte.setCustomValidity("");
-}
+  function tractaError(objecte, text) {
+    objecte.setCustomValidity(text);
+    objecte.reportValidity();
+  }
 
-function tractaError(objecte, text) {
-  objecte.setCustomValidity(text);
-  objecte.reportValidity()
-}
+  function mostrarOk(obj) {
+    obj.nextElementSibling.style.display = "block";
+  }
 
-function mostrarOk(obj) {
-  obj.nextElementSibling.style.display = "block";
-}
-
-function ocultarOk(obj) {
-  obj.nextElementSibling.style.display = "none";
-}
-}
+  function ocultarOk(obj) {
+    obj.nextElementSibling.style.display = "none";
+  }
+};
---
+++ registre.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Inscripció Institut Marianao</title>
+        <meta charset="utf-8">
+        <link href="estils.css" rel="stylesheet" type="text/css">
+    </head>
+    <body> 
+        <h1 id="titol">Inscripció a Cicles d'Informàtica</h1>
+        <h2>Institut Marianao</h2>
+        <?php
+        if (isset($_POST['email'])) {
+            define('DB_SERVER', 'localhost');
+            define('DB_USERNAME', 'cfgs');
+            define('DB_PASSWORD', 'ira491');
+            define('DB_DATABASE', 'cfgs');
+            $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+            $nom = utf8_decode($_POST["nom"]);
+            $cognom1 = utf8_decode($_POST["cognom1"]);
+            $cognom2 = utf8_decode($_POST["cognom2"]);
+            $data = $_POST["datanaixement"];
+            $mail = $_POST["email"];
+            $tlf = $_POST["tel"];
+            $dni = $_POST["dni"];
+            $dropdown = utf8_decode($_POST["cicle"]);
+            $radio = utf8_decode($_POST["radio"]);
+            $usuari = utf8_decode($_POST["usuari"]);
+            $passwd1 = $_POST["passwd1"];
+            $encryptPassword = password_hash($passwd1, PASSWORD_DEFAULT);
+            $textarea = utf8_decode($_POST["textarea"]);
+
+            $stmt = $db->prepare("SELECT MAIL FROM register WHERE MAIL = ?");
+            $stmt->bind_param("s", $mail);
+            $stmt->execute();
+            $stmt->bind_result($userResult);
+            $stmt->fetch();
+            $stmt->close();
+
+            if ($userResult == $mail) {
+                echo "<section class='resultat'>";
+                echo "<h3>Error: </h3>";
+                echo "<p>Inscripció ja existeix. eMail: " . $mail . "</p>";
+                echo "</section>";
+            } else {
+                $stmt = $db->prepare("INSERT INTO register(NOM, COGNOM1, COGNOM2, NAIXEMENT, MAIL, TELEFON, DNI, CICLEFORMATIU, ESTUDIS, USUARI, PASSWORD, OBSERVACIONS) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+                if (!$stmt) {
+                    echo $db->error;
+                }
+
+                $stmt->bind_param("sssssissssss", $nom, $cognom1, $cognom2, $data, $mail, $tlf, $dni, $dropdown, $radio, $usuari, $encryptPassword, $textarea);
+                $stmt->execute();
+                $result = $stmt->affected_rows;
+                $lit_err = $db->error;
+                $stmt->close();
+                $db->close();
+
+                if ($result <= 0) {
+                    echo "<section class='resultat'>";
+                    echo "<h3>Error: </h3>";
+                    echo "<p>Error a l'insertar registre a la BBDD: " . $lit_err . "</p>";
+                    echo "</section>";
+                } else {
+                    echo "<section class='resultat'>";
+                    echo "<h3>Inscripció correcta: </h3>";
+                    echo "<strong>Nom usuari</strong>: " . $_POST['usuari'] . "<br>";
+                    echo "<strong>Mail</strong>: " . $_POST['email'] . "<br>";
+                    echo "</section>";
+                }
+            }
+        } else {
+            echo "<section class='resultat'>";
+            echo "<h3>Error: </h3>";
+            echo "<p>Error. Falten dades. " . $lit_err . "</p>";
+            echo "</section>";
+        }
+        ?>
+    </body>
+</html>
+
